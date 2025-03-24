360b1f9808a4784b784b67e3ef09db28de06e2cd
grzegorz <you@example.com>
2025-03-24 22:52:51 +0100

    process up

--- datasource/person_ds.py
+++ datasource/person_ds.py
@@ -1,18 +1,23 @@
 from dataclasses import dataclass
 from utils.random import random_id
 import math
-import asyncio
+from multiprocessing import Process, Queue
 
-def is_prime(n: int) -> bool:
+def is_prime(n: int, q: Queue) -> bool:
     if n < 2:
+        q.put(False)
         return False
     if n == 2:
+        q.put(True)
         return True
     if n%2 == 0:
+        q.put(False)
         return False
     for i in range(3, math.isqrt(n) + 1, 2):
         if n%i == 0:
+            q.put(False)
             return False
+    q.put(True)
     return True
 
 @dataclass
@@ -33,5 +38,12 @@ class PersonDS():
 
     @random_id
     def get_person(self, id):
+        
+        q = Queue()
+        p = Process(target=is_prime, args=(5000111000222021, q))
+        p.start()
+        p.join()
+        print("PrimeResult: ", q.get())
+
         # is_prime(5000111000222021) # ostro pali proca
         return self.__persons[id] if id in self.__persons else None
